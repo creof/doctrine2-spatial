b68b23270b6b0a22c698376f2ff1d5834e48bc88
Derek Lambert <dlambert@dereklambert.com>
2012-11-05 19:03:12 -0600

    Const was moved.

--- lib/CrEOF/Spatial/DBAL/Types/GeographyType.php
+++ lib/CrEOF/Spatial/DBAL/Types/GeographyType.php
@@ -23,7 +23,7 @@
 
 namespace CrEOF\Spatial\DBAL\Types;
 
-use CrEOF\Spatial\PHP\Types\AbstractGeometry;
+use CrEOF\Spatial\PHP\Types\Geography\GeographyInterface;
 
 /**
  * Doctrine GEOGRAPHY type
@@ -33,8 +33,11 @@ use CrEOF\Spatial\PHP\Types\AbstractGeometry;
  */
 class GeographyType extends AbstractGeometryType
 {
+    /**
+     * {@inheritdoc}
+     */
     public function getBaseType()
     {
-        return AbstractGeometry::GEOGRAPHY;
+        return GeographyInterface::GEOGRAPHY;
     }
 }
