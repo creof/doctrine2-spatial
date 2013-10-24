7f5a01087d4c2e7f818c29ed669b9f2dfce7763c
Derek Lambert <dlambert@dereklambert.com>
2013-10-24 18:29:13 -0500

    This was a mess!

--- tests/CrEOF/Spatial/Tests/OrmTest.php
+++ tests/CrEOF/Spatial/Tests/OrmTest.php
@@ -52,11 +52,6 @@ abstract class OrmTest extends \Doctrine\Tests\OrmFunctionalTestCase
      */
     protected $conn;
 
-    /**
-     * @var string
-     */
-    protected $platformName;
-
     const GEOMETRY_ENTITY         = 'CrEOF\Spatial\Tests\Fixtures\GeometryEntity';
     const NO_HINT_GEOMETRY_ENTITY = 'CrEOF\Spatial\Tests\Fixtures\NoHintGeometryEntity';
     const POINT_ENTITY            = 'CrEOF\Spatial\Tests\Fixtures\PointEntity';
@@ -71,80 +66,54 @@ abstract class OrmTest extends \Doctrine\Tests\OrmFunctionalTestCase
     {
         parent::setUp();
 
-        $this->conn         = static::$_sharedConn;
-        $this->platformName = $this->conn->getDatabasePlatform()->getName();
+        $this->conn = static::$_sharedConn;
 
-        /**
-         * Add types to DBAL and setup fixtures
-         */
         if ( ! static::$_setup) {
+            $this->conn->getConfiguration()->setSQLLogger(new \CrEOF\Spatial\Tests\FileSQLLogger('/Users/dlambert/Development/doctrine2-spatial/logger.log'));
+
             static::$_setup = true;
 
-            switch ($this->platformName) {
-                case 'postgresql':
-                    $this->setUpPostgreSql();
-                    break;
-                case 'mysql':
-                    $this->setUpMySql();
-                    break;
-                default:
-                    throw UnsupportedPlatformException::unsupportedPlatform($this->platformName);
-                    break;
-            }
+            $this->setupPlatform();
+            $this->setupTypes();
+            $this->setupEntities();
         }
 
-        /**
-         * Add DQL functions to ORM
-         */
-        $this->setupCommonFunctions();
+        $this->setupFunctions();
+    }
 
-        switch ($this->platformName) {
+    /**
+     * Perform any platform specific setup
+     *
+     * @group mysql
+     *
+     * @throws \CrEOF\Spatial\Exception\UnsupportedPlatformException
+     */
+    protected function setupPlatform()
+    {
+        switch ($this->getPlatform()->getName()) {
             case 'postgresql':
-                $this->setUpPostgreSqlFunctions();
+                $this->conn->exec('CREATE EXTENSION postgis');
                 break;
             case 'mysql':
-                $this->setUpMySqlFunctions();
                 break;
             default:
-                throw UnsupportedPlatformException::unsupportedPlatform($this->platformName);
+                throw UnsupportedPlatformException::unsupportedPlatform($this->getPlatform()->getName());
                 break;
         }
     }
 
     /**
-     * @throws UnsupportedPlatformException
+     * Add types to DBAL
      */
-    protected function tearDown()
+    protected function setupTypes()
     {
-        parent::tearDown();
-
-        $this->_sqlLoggerStack->enabled = false;
-
-        $this->tearDownCommonEntities();
-
-        switch ($this->platformName) {
-            case 'postgresql':
-                $this->tearDownPostgreSql();
-                break;
-            case 'mysql':
-                break;
-            default:
-                throw UnsupportedPlatformException::unsupportedPlatform($this->platformName);
-                break;
-        }
-
-        $this->_em->clear();
-    }
-
-    /**
-     * Add types to DBAL common to all platforms
-     */
-    protected function setupCommonTypes()
-    {
+        // Geometry
         Type::addType('geometry', 'CrEOF\Spatial\DBAL\Types\GeometryType');
         Type::addType('point', 'CrEOF\Spatial\DBAL\Types\Geometry\PointType');
         Type::addType('linestring', 'CrEOF\Spatial\DBAL\Types\Geometry\LineStringType');
         Type::addType('polygon', 'CrEOF\Spatial\DBAL\Types\Geometry\PolygonType');
+
+        // Geography
         Type::addType('geography', 'CrEOF\Spatial\DBAL\Types\GeographyType');
         Type::addType('geographypoint', 'CrEOF\Spatial\DBAL\Types\Geography\PointType');
         Type::addType('geographylinestring', 'CrEOF\Spatial\DBAL\Types\Geography\LineStringType');
@@ -152,93 +121,93 @@ abstract class OrmTest extends \Doctrine\Tests\OrmFunctionalTestCase
     }
 
     /**
-     * Add DQL functions to ORM common to all platforms
+     * Setup fixtures
      */
-    protected function setupCommonFunctions()
-    {
-    }
-
-    /**
-     * Setup fixtures common to all platforms
-     */
-    protected function setupCommonEntities()
+    protected function setupEntities()
     {
         $this->_schemaTool->createSchema(
             array(
+                // Geometry
                 $this->_em->getClassMetadata(self::GEOMETRY_ENTITY),
                 $this->_em->getClassMetadata(self::NO_HINT_GEOMETRY_ENTITY),
                 $this->_em->getClassMetadata(self::POINT_ENTITY),
                 $this->_em->getClassMetadata(self::LINESTRING_ENTITY),
                 $this->_em->getClassMetadata(self::POLYGON_ENTITY),
+
+                // Geography
                 $this->_em->getClassMetadata(self::GEOGRAPHY_ENTITY)
             )
         );
     }
 
-    protected function tearDownCommonEntities()
+    /**
+     * Setup DQL functions
+     */
+    protected function setUpFunctions()
     {
+        if ($this->getPlatform()->getName() == 'postgresql') {
+            $this->_em->getConfiguration()->addCustomStringFunction('st_asbinary', 'CrEOF\Spatial\ORM\Query\AST\Functions\PostgreSql\STAsBinary');
+            $this->_em->getConfiguration()->addCustomStringFunction('st_astext', 'CrEOF\Spatial\ORM\Query\AST\Functions\PostgreSql\STAsText');
+            $this->_em->getConfiguration()->addCustomNumericFunction('st_area', 'CrEOF\Spatial\ORM\Query\AST\Functions\PostgreSql\STArea');
+            $this->_em->getConfiguration()->addCustomStringFunction('st_centroid', 'CrEOF\Spatial\ORM\Query\AST\Functions\PostgreSql\STCentroid');
+            $this->_em->getConfiguration()->addCustomStringFunction('st_closestpoint', 'CrEOF\Spatial\ORM\Query\AST\Functions\PostgreSql\STClosestPoint');
+            $this->_em->getConfiguration()->addCustomNumericFunction('st_contains', 'CrEOF\Spatial\ORM\Query\AST\Functions\PostgreSql\STContains');
+            $this->_em->getConfiguration()->addCustomNumericFunction('st_containsproperly', 'CrEOF\Spatial\ORM\Query\AST\Functions\PostgreSql\STContainsProperly');
+            $this->_em->getConfiguration()->addCustomNumericFunction('st_covers', 'CrEOF\Spatial\ORM\Query\AST\Functions\PostgreSql\STCovers');
+            $this->_em->getConfiguration()->addCustomNumericFunction('st_coveredby', 'CrEOF\Spatial\ORM\Query\AST\Functions\PostgreSql\STCoveredBy');
+            $this->_em->getConfiguration()->addCustomNumericFunction('st_crosses', 'CrEOF\Spatial\ORM\Query\AST\Functions\PostgreSql\STCrosses');
+            $this->_em->getConfiguration()->addCustomNumericFunction('st_disjoint', 'CrEOF\Spatial\ORM\Query\AST\Functions\PostgreSql\STDisjoint');
+            $this->_em->getConfiguration()->addCustomNumericFunction('st_distance', 'CrEOF\Spatial\ORM\Query\AST\Functions\PostgreSql\STDistance');
+            $this->_em->getConfiguration()->addCustomStringFunction('st_envelope', 'CrEOF\Spatial\ORM\Query\AST\Functions\PostgreSql\STEnvelope');
+            $this->_em->getConfiguration()->addCustomStringFunction('st_geomfromtext', 'CrEOF\Spatial\ORM\Query\AST\Functions\PostgreSql\STGeomFromText');
+            $this->_em->getConfiguration()->addCustomNumericFunction('st_length', 'CrEOF\Spatial\ORM\Query\AST\Functions\PostgreSql\STLength');
+            $this->_em->getConfiguration()->addCustomNumericFunction('st_linecrossingdirection', 'CrEOF\Spatial\ORM\Query\AST\Functions\PostgreSql\STLineCrossingDirection');
+            $this->_em->getConfiguration()->addCustomStringFunction('st_startpoint', 'CrEOF\Spatial\ORM\Query\AST\Functions\PostgreSql\STStartPoint');
+            $this->_em->getConfiguration()->addCustomStringFunction('st_summary', 'CrEOF\Spatial\ORM\Query\AST\Functions\PostgreSql\STSummary');
+        }
+
+        if ($this->getPlatform()->getName() == 'mysql') {
+            $this->_em->getConfiguration()->addCustomNumericFunction('area', 'CrEOF\Spatial\ORM\Query\AST\Functions\MySql\Area');
+            $this->_em->getConfiguration()->addCustomStringFunction('asbinary', 'CrEOF\Spatial\ORM\Query\AST\Functions\MySql\AsBinary');
+            $this->_em->getConfiguration()->addCustomStringFunction('astext', 'CrEOF\Spatial\ORM\Query\AST\Functions\MySql\AsText');
+            $this->_em->getConfiguration()->addCustomNumericFunction('contains', 'CrEOF\Spatial\ORM\Query\AST\Functions\MySql\Contains');
+            $this->_em->getConfiguration()->addCustomNumericFunction('disjoint', 'CrEOF\Spatial\ORM\Query\AST\Functions\MySql\Disjoint');
+            $this->_em->getConfiguration()->addCustomStringFunction('envelope', 'CrEOF\Spatial\ORM\Query\AST\Functions\MySql\Envelope');
+            $this->_em->getConfiguration()->addCustomStringFunction('geomfromtext', 'CrEOF\Spatial\ORM\Query\AST\Functions\MySql\GeomFromText');
+            $this->_em->getConfiguration()->addCustomNumericFunction('glength', 'CrEOF\Spatial\ORM\Query\AST\Functions\MySql\GLength');
+            $this->_em->getConfiguration()->addCustomNumericFunction('mbrcontains', 'CrEOF\Spatial\ORM\Query\AST\Functions\MySql\MBRContains');
+            $this->_em->getConfiguration()->addCustomNumericFunction('mbrdisjoint', 'CrEOF\Spatial\ORM\Query\AST\Functions\MySql\MBRDisjoint');
+            $this->_em->getConfiguration()->addCustomStringFunction('startpoint', 'CrEOF\Spatial\ORM\Query\AST\Functions\MySql\StartPoint');
+        }
+    }
+
+    /**
+     * Teardown fixtures
+     */
+    protected function tearDown()
+    {
+        parent::tearDown();
+
+        $this->_sqlLoggerStack->enabled = false;
+
+        $this->tearDownEntities();
+        $this->_em->clear();
+    }
+
+    /**
+     * Teardown entities
+     */
+    protected function tearDownEntities()
+    {
+        // Geometry
         $this->conn->executeUpdate('DELETE FROM GeometryEntity');
+        $this->conn->executeUpdate('DELETE FROM NoHintGeometryEntity');
         $this->conn->executeUpdate('DELETE FROM PointEntity');
         $this->conn->executeUpdate('DELETE FROM LineStringEntity');
         $this->conn->executeUpdate('DELETE FROM PolygonEntity');
+
+        // Geography
         $this->conn->executeUpdate('DELETE FROM GeographyEntity');
-    }
-
-    protected function setUpMySql()
-    {
-        $this->setupCommonTypes();
-        $this->setupCommonEntities();
-    }
-
-    protected function setUpPostgreSql()
-    {
-        $this->conn->exec('CREATE EXTENSION postgis');
-
-        $this->setupCommonTypes();
-        $this->setupCommonFunctions();
-        $this->setupCommonEntities();
-    }
-
-    protected function setUpPostgreSqlFunctions()
-    {
-        $this->_em->getConfiguration()->addCustomStringFunction('st_asbinary', 'CrEOF\Spatial\ORM\Query\AST\Functions\PostgreSql\STAsBinary');
-        $this->_em->getConfiguration()->addCustomStringFunction('st_astext', 'CrEOF\Spatial\ORM\Query\AST\Functions\PostgreSql\STAsText');
-        $this->_em->getConfiguration()->addCustomNumericFunction('st_area', 'CrEOF\Spatial\ORM\Query\AST\Functions\PostgreSql\STArea');
-        $this->_em->getConfiguration()->addCustomStringFunction('st_centroid', 'CrEOF\Spatial\ORM\Query\AST\Functions\PostgreSql\STCentroid');
-        $this->_em->getConfiguration()->addCustomStringFunction('st_closestpoint', 'CrEOF\Spatial\ORM\Query\AST\Functions\PostgreSql\STClosestPoint');
-        $this->_em->getConfiguration()->addCustomNumericFunction('st_contains', 'CrEOF\Spatial\ORM\Query\AST\Functions\PostgreSql\STContains');
-        $this->_em->getConfiguration()->addCustomNumericFunction('st_containsproperly', 'CrEOF\Spatial\ORM\Query\AST\Functions\PostgreSql\STContainsProperly');
-        $this->_em->getConfiguration()->addCustomNumericFunction('st_covers', 'CrEOF\Spatial\ORM\Query\AST\Functions\PostgreSql\STCovers');
-        $this->_em->getConfiguration()->addCustomNumericFunction('st_coveredby', 'CrEOF\Spatial\ORM\Query\AST\Functions\PostgreSql\STCoveredBy');
-        $this->_em->getConfiguration()->addCustomNumericFunction('st_crosses', 'CrEOF\Spatial\ORM\Query\AST\Functions\PostgreSql\STCrosses');
-        $this->_em->getConfiguration()->addCustomNumericFunction('st_disjoint', 'CrEOF\Spatial\ORM\Query\AST\Functions\PostgreSql\STDisjoint');
-        $this->_em->getConfiguration()->addCustomNumericFunction('st_distance', 'CrEOF\Spatial\ORM\Query\AST\Functions\PostgreSql\STDistance');
-        $this->_em->getConfiguration()->addCustomStringFunction('st_envelope', 'CrEOF\Spatial\ORM\Query\AST\Functions\PostgreSql\STEnvelope');
-        $this->_em->getConfiguration()->addCustomStringFunction('st_geomfromtext', 'CrEOF\Spatial\ORM\Query\AST\Functions\PostgreSql\STGeomFromText');
-        $this->_em->getConfiguration()->addCustomNumericFunction('st_length', 'CrEOF\Spatial\ORM\Query\AST\Functions\PostgreSql\STLength');
-        $this->_em->getConfiguration()->addCustomNumericFunction('st_linecrossingdirection', 'CrEOF\Spatial\ORM\Query\AST\Functions\PostgreSql\STLineCrossingDirection');
-        $this->_em->getConfiguration()->addCustomStringFunction('st_startpoint', 'CrEOF\Spatial\ORM\Query\AST\Functions\PostgreSql\STStartPoint');
-        $this->_em->getConfiguration()->addCustomStringFunction('st_summary', 'CrEOF\Spatial\ORM\Query\AST\Functions\PostgreSql\STSummary');
-    }
-
-    protected function setUpMySqlFunctions()
-    {
-        $this->_em->getConfiguration()->addCustomNumericFunction('area', 'CrEOF\Spatial\ORM\Query\AST\Functions\MySql\Area');
-        $this->_em->getConfiguration()->addCustomStringFunction('asbinary', 'CrEOF\Spatial\ORM\Query\AST\Functions\MySql\AsBinary');
-        $this->_em->getConfiguration()->addCustomStringFunction('astext', 'CrEOF\Spatial\ORM\Query\AST\Functions\MySql\AsText');
-        $this->_em->getConfiguration()->addCustomNumericFunction('contains', 'CrEOF\Spatial\ORM\Query\AST\Functions\MySql\Contains');
-        $this->_em->getConfiguration()->addCustomNumericFunction('disjoint', 'CrEOF\Spatial\ORM\Query\AST\Functions\MySql\Disjoint');
-        $this->_em->getConfiguration()->addCustomStringFunction('envelope', 'CrEOF\Spatial\ORM\Query\AST\Functions\MySql\Envelope');
-        $this->_em->getConfiguration()->addCustomStringFunction('geomfromtext', 'CrEOF\Spatial\ORM\Query\AST\Functions\MySql\GeomFromText');
-        $this->_em->getConfiguration()->addCustomNumericFunction('glength', 'CrEOF\Spatial\ORM\Query\AST\Functions\MySql\GLength');
-        $this->_em->getConfiguration()->addCustomNumericFunction('mbrcontains', 'CrEOF\Spatial\ORM\Query\AST\Functions\MySql\MBRContains');
-        $this->_em->getConfiguration()->addCustomNumericFunction('mbrdisjoint', 'CrEOF\Spatial\ORM\Query\AST\Functions\MySql\MBRDisjoint');
-        $this->_em->getConfiguration()->addCustomStringFunction('startpoint', 'CrEOF\Spatial\ORM\Query\AST\Functions\MySql\StartPoint');
-    }
-
-    protected function tearDownPostgreSql()
-    {
-
     }
 
     /**
