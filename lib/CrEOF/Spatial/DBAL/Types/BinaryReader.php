0a0b07d4f7064505a5359b90da728c303d6019e7
Derek Lambert <dlambert@dereklambert.com>
2014-03-13 06:19:32 -0500

    Update license date

--- lib/CrEOF/Spatial/DBAL/Types/BinaryReader.php
+++ lib/CrEOF/Spatial/DBAL/Types/BinaryReader.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (C) 2012 Derek J. Lambert
+ * Copyright (C) 2012, 2014 Derek J. Lambert
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
