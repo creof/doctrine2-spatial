3e1f46c3109efca8724283a5c87624d96d7fd949
Luca Capra <luca.capra@gmail.com>
2015-10-20 14:51:08 +0200

    revert original copyright

--- lib/CrEOF/Spatial/ORM/Query/AST/Functions/MySql/Contains.php
+++ lib/CrEOF/Spatial/ORM/Query/AST/Functions/MySql/Contains.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (C) 2013 luca capra
+ * Copyright (C) 2012 Derek J. Lambert
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -28,7 +28,7 @@ use CrEOF\Spatial\ORM\Query\AST\Functions\AbstractSpatialDQLFunction;
 /**
  * Contains DQL function
  *
- * @author  luca capra <luca.capra@gmail.com>
+ * @author  Derek J. Lambert <dlambert@dereklambert.com>
  * @license http://dlambert.mit-license.org MIT
  */
 class Contains extends AbstractSpatialDQLFunction
