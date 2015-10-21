2e6dfcabdb040b363c7ae0c998f5431ab9b446d3
Derek J. Lambert <dlambert@dereklambert.com>
2015-10-21 10:40:10 -0500

    Remove redundant tests and correct expected values

--- tests/CrEOF/Spatial/Tests/ORM/Query/AST/Functions/PostgreSql/STDistanceSphereTest.php
+++ tests/CrEOF/Spatial/Tests/ORM/Query/AST/Functions/PostgreSql/STDistanceSphereTest.php
@@ -44,15 +44,13 @@ class STDistanceSphereTest extends OrmTest
     protected function setUp()
     {
         $this->useEntity('point');
-        $this->useEntity('geography');
-        $this->useType('geopoint');
         parent::setUp();
     }
 
     /**
      * @group geometry
      */
-    public function testSelectSTDistanceSphereGeometryCartesian()
+    public function testSelectSTDistanceSphereGeometry()
     {
         $newYork   = new Point(-73.938611, 40.664167);
         $losAngles = new Point(-118.2430, 34.0522);
@@ -79,90 +77,6 @@ class STDistanceSphereTest extends OrmTest
         $query = $this->_em->createQuery('SELECT p, ST_Distance_Sphere(p.point, ST_GeomFromText(:p1)) FROM CrEOF\Spatial\Tests\Fixtures\PointEntity p');
 
         $query->setParameter('p1', $madison, 'point');
-
-        $result = $query->getResult();
-
-        $this->assertCount(3, $result);
-        $this->assertEquals($entity1, $result[0][0]);
-        $this->assertEquals(15.646934398128, $result[0][1]);
-        $this->assertEquals($entity2, $result[1][0]);
-        $this->assertEquals(30.2188561049899, $result[1][1]);
-        $this->assertEquals($entity3, $result[2][0]);
-        $this->assertEquals(12.6718564262953, $result[2][1]);
-    }
-
-    /**
-     * @group geography
-     */
-    public function testSelectSTDistanceSphereGeographySpheroid()
-    {
-        $newYork   = new GeographyPoint(-73.938611, 40.664167);
-        $losAngles = new GeographyPoint(-118.2430, 34.0522);
-        $dallas    = new GeographyPoint(-96.803889, 32.782778);
-        $madison   = new GeographyPoint(-89.4, 43.066667);
-
-        $entity1 = new GeographyEntity();
-
-        $entity1->setGeography($newYork);
-        $this->_em->persist($entity1);
-
-        $entity2 = new GeographyEntity();
-
-        $entity2->setGeography($losAngles);
-        $this->_em->persist($entity2);
-
-        $entity3 = new GeographyEntity();
-
-        $entity3->setGeography($dallas);
-        $this->_em->persist($entity3);
-        $this->_em->flush();
-        $this->_em->clear();
-
-        $query = $this->_em->createQuery('SELECT g, ST_Distance_Sphere(g.geography, ST_GeomFromText(:p1)) FROM CrEOF\Spatial\Tests\Fixtures\GeographyEntity g');
-
-        $query->setParameter('p1', $madison, 'geopoint');
-
-        $result = $query->getResult();
-
-        $this->assertCount(3, $result);
-        $this->assertEquals($entity1, $result[0][0]);
-        $this->assertEquals(1309106.31457703, $result[0][1]);
-        $this->assertEquals($entity2, $result[1][0]);
-        $this->assertEquals(2689041.41286683, $result[1][1]);
-        $this->assertEquals($entity3, $result[2][0]);
-        $this->assertEquals(1312731.61416563, $result[2][1]);
-    }
-
-    /**
-     * @group geography
-     */
-    public function testSelectSTDistanceSphereGeographyCartesian()
-    {
-        $newYork   = new GeographyPoint(-73.938611, 40.664167);
-        $losAngles = new GeographyPoint(-118.2430, 34.0522);
-        $dallas    = new GeographyPoint(-96.803889, 32.782778);
-        $madison   = new GeographyPoint(-89.4, 43.066667);
-
-        $entity1 = new GeographyEntity();
-
-        $entity1->setGeography($newYork);
-        $this->_em->persist($entity1);
-
-        $entity2 = new GeographyEntity();
-
-        $entity2->setGeography($losAngles);
-        $this->_em->persist($entity2);
-
-        $entity3 = new GeographyEntity();
-
-        $entity3->setGeography($dallas);
-        $this->_em->persist($entity3);
-        $this->_em->flush();
-        $this->_em->clear();
-
-        $query = $this->_em->createQuery('SELECT g, ST_Distance_Sphere(g.geography, ST_GeomFromText(:p1), false) FROM CrEOF\Spatial\Tests\Fixtures\GeographyEntity g');
-
-        $query->setParameter('p1', $madison, 'geopoint');
 
         $result = $query->getResult();
 
