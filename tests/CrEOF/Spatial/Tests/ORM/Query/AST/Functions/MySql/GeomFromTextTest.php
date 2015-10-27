b69c424e4dfb444d5c9ee6ab04938ebe6bcd52f1
Derek J. Lambert <dlambert@dereklambert.com>
2015-10-27 07:21:56 -0500

    Test for text, don't presume we know what the object does.

--- tests/CrEOF/Spatial/Tests/ORM/Query/AST/Functions/MySql/GeomFromTextTest.php
+++ tests/CrEOF/Spatial/Tests/ORM/Query/AST/Functions/MySql/GeomFromTextTest.php
@@ -62,7 +62,7 @@ class GeomFromTextTest extends OrmTest
 
         $query = $this->getEntityManager()->createQuery('SELECT g FROM CrEOF\Spatial\Tests\Fixtures\GeometryEntity g WHERE g.geometry = GeomFromText(:geometry)');
 
-        $query->setParameter('geometry', new Point(5, 5), 'point');
+        $query->setParameter('geometry', 'POINT(5 5)', 'string');
 
         $result = $query->getResult();
 
@@ -90,7 +90,7 @@ class GeomFromTextTest extends OrmTest
 
         $query = $this->getEntityManager()->createQuery('SELECT g FROM CrEOF\Spatial\Tests\Fixtures\GeometryEntity g WHERE g.geometry = GeomFromText(:geometry)');
 
-        $query->setParameter('geometry', new LineString($value), 'linestring');
+        $query->setParameter('geometry', 'LINESTRING(0 0,5 5,10 10)', 'string');
 
         $result = $query->getResult();
 
