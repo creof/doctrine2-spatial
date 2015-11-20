f4ad7aee27b9398d98b016ad2ca9991127ac460e
Derek J. Lambert <dlambert@dereklambert.com>
2015-11-20 09:59:53 -0600

    Test for geography point with SRID in column definition.

---
+++ tests/CrEOF/Spatial/Tests/DBAL/Types/Geography/GeoPointSridTest.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Copyright (C) 2015 Derek J. Lambert
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace CrEOF\Spatial\Tests\DBAL\Types\Geography;
+
+use Doctrine\ORM\Query;
+use CrEOF\Spatial\PHP\Types\Geography\Point;
+use CrEOF\Spatial\Tests\OrmTest;
+use CrEOF\Spatial\Tests\Fixtures\GeoPointSridEntity;
+
+/**
+ * Doctrine GeographyType tests
+ *
+ * @author  Derek J. Lambert <dlambert@dereklambert.com>
+ * @license http://dlambert.mit-license.org MIT
+ *
+ * @group srid
+ */
+class GeoPointSridTest extends OrmTest
+{
+    protected function setUp()
+    {
+        $this->usesEntity(self::GEO_POINT_SRID_ENTITY);
+        parent::setUp();
+    }
+
+    public function testNullGeography()
+    {
+        $entity = new GeoPointSridEntity();
+
+        $this->getEntityManager()->persist($entity);
+        $this->getEntityManager()->flush();
+
+        $id = $entity->getId();
+
+        $this->getEntityManager()->clear();
+
+        $queryEntity = $this->getEntityManager()->getRepository(self::GEO_POINT_SRID_ENTITY)->find($id);
+
+        $this->assertEquals($entity, $queryEntity);
+    }
+
+    public function testPointGeography()
+    {
+        $entity = new GeoPointSridEntity();
+
+        $entity->setPoint(new Point(11, 11));
+        $this->getEntityManager()->persist($entity);
+        $this->getEntityManager()->flush();
+
+        $id = $entity->getId();
+
+        $this->getEntityManager()->clear();
+
+        $queryEntity = $this->getEntityManager()->getRepository(self::GEO_POINT_SRID_ENTITY)->find($id);
+
+        $this->assertEquals($entity, $queryEntity);
+        $this->assertEquals(4326, $queryEntity->getPoint()->getSrid());
+    }
+}
