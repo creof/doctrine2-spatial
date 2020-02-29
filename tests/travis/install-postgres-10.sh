04788ad69c037b3ecae699f91e5e06150c73ad8e
Alexandre-T <alexandre.tranchant@gmail.com>
2020-02-29 23:28:43 +0100

    Updating Travis and installing postgis on postgresql10

--- tests/travis/install-postgres-10.sh
+++ tests/travis/install-postgres-10.sh
@@ -6,7 +6,7 @@ echo "Installing Postgres 10"
 sudo service postgresql stop
 sudo apt-get remove -q 'postgresql-*'
 sudo apt-get update -q
-sudo apt-get install -q postgresql-10 postgresql-client-10
+sudo apt-get install -q postgresql-10 postgresql-client-10 postgis postgresql-10-postgis-2.4
 sudo cp /etc/postgresql/{9.6,10}/main/pg_hba.conf
 
 echo "Restarting Postgres 10"
