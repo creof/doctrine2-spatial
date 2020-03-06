e00f30b6721900cd756f023b3c7e7601335a2d48
Alexandre-T <alexandre.tranchant@gmail.com>
2020-03-06 15:08:37 +0100

    Fixing test

--- tests/CrEOF/Spatial/Tests/ORM/Query/AST/Functions/PostgreSql/SpTransformTest.php
+++ tests/CrEOF/Spatial/Tests/ORM/Query/AST/Functions/PostgreSql/SpTransformTest.php
@@ -76,20 +76,6 @@ class SpTransformTest extends OrmTestCase
         $massachusetts = $this->createMassachusettsState();
         $this->getEntityManager()->flush();
         $this->getEntityManager()->clear();
-
-        //FIXME The test above failed because DQL SRID is seen as a string
-//        $query = $this->getEntityManager()->createQuery(
-//            // phpcs:disable Generic.Files.LineLength.MaxExceeded
-//            'SELECT p, ST_AsText(PgSql_Transform(p.polygon, :srid)) FROM CrEOF\Spatial\Tests\Fixtures\PolygonEntity p'
-//            // phpccs: enable
-//        );
-//        $query->setParameter('srid', 4326, 'integer');
-//        $result = $query->getResult();
-//
-//        static::assertCount(2, $result);
-//        static::assertEquals($test, $result[0][0]);
-//        static::assertSame('POLYGON((-71.1776848522251 42.3902896512902,-71.1776843766326 42.3903829478009, -71.1775844305465 42.3903826677917,-71.1775825927231 42.3902893647987,-71.1776848522251 42.3902896512902))', $result[0][1]);
-
         $query = $this->getEntityManager()->createQuery(
             // phpcs:disable Generic.Files.LineLength.MaxExceeded
             'SELECT p, ST_AsText(PgSql_Transform(p.polygon, :proj)) FROM CrEOF\Spatial\Tests\Fixtures\PolygonEntity p'
@@ -104,7 +90,7 @@ class SpTransformTest extends OrmTestCase
     }
 
     /**
-     * Test a DQL containing function to test in the predicate.
+     * Test a DQL containing function to test in the select.
      *
      * @throws DBALException                when connection failed
      * @throws ORMException                 when cache is not set
@@ -113,23 +99,55 @@ class SpTransformTest extends OrmTestCase
      *
      * @group geometry
      */
-    public function testFunctionInPredicate()
+    public function testFunctionInSelectWith3Parameters()
+    {
+        // phpcs:disable Generic.Files.LineLength.MaxExceeded
+        $massachusetts = $this->createMassachusettsState(false);
+        $this->getEntityManager()->flush();
+        $this->getEntityManager()->clear();
+        $query = $this->getEntityManager()->createQuery(
+            'SELECT p, ST_AsText(PgSql_Transform(p.polygon, :from, :to)) FROM CrEOF\Spatial\Tests\Fixtures\PolygonEntity p'
+        );
+        $query->setParameter('from', '+proj=lcc +lat_1=42.68333333333333 +lat_2=41.71666666666667 +lat_0=41 +lon_0=-71.5 +x_0=200000.0001016002 +y_0=750000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=us-ft +no_defs ');
+        $query->setParameter('to', '+proj=longlat +datum=WGS84 +no_defs');
+        $result = $query->getResult();
+
+        static::assertCount(1, $result);
+        static::assertEquals($massachusetts, $result[0][0]);
+        static::assertSame('POLYGON((-71.1776848522251 42.3902896512902,-71.1776843766326 42.3903829478009,-71.1775844305465 42.3903826677917,-71.1775825927231 42.3902893647987,-71.1776848522251 42.3902896512902))', $result[0][1]);
+        // phpccs: enable
+    }
+
+    /**
+     * Test a DQL containing function to test in the select.
+     *
+     * @throws DBALException                when connection failed
+     * @throws ORMException                 when cache is not set
+     * @throws UnsupportedPlatformException when platform is unsupported
+     * @throws InvalidValueException        when geometries are not valid
+     *
+     * @group geometry
+     */
+    public function testFunctionInSelectWithSrid()
     {
         $massachusetts = $this->createMassachusettsState();
-        $this->createSmallPolygon();
         $this->getEntityManager()->flush();
         $this->getEntityManager()->clear();
 
-        // phpcs:disable Generic.Files.LineLength.MaxExceeded
+        //FIXME The test above failed because DQL SRID is seen as a string
+        self::markTestSkipped('The test above failed because DQL SRID is seen as a string');
         $query = $this->getEntityManager()->createQuery(
-            'SELECT p FROM CrEOF\Spatial\Tests\Fixtures\PolygonEntity p WHERE PgSql_Transform(p.polygon, :proj) = ST_GeomFromText(:g)'
+            // phpcs:disable Generic.Files.LineLength.MaxExceeded
+            'SELECT p, ST_AsText(PgSql_Transform(p.polygon, :srid)) FROM CrEOF\Spatial\Tests\Fixtures\PolygonEntity p'
+            // phpccs: enable
         );
-        $query->setParameter('proj', '+proj=longlat +datum=WGS84 +no_defs');
-        $query->setParameter('g', 'POLYGON((-71.1776848522251 42.3902896512902,-71.1776843766326 42.3903829478009,-71.1775844305465 42.3903826677917,-71.1775825927231 42.3902893647987,-71.1776848522251 42.3902896512902))', 'string');
+        $query->setParameter('srid', 4326, 'integer');
         $result = $query->getResult();
-        // phpcs:enable
 
         static::assertCount(1, $result);
-        static::assertEquals($massachusetts, $result[0]);
+        static::assertEquals($massachusetts, $result[0][0]);
+        // phpcs:disable Generic.Files.LineLength.MaxExceeded
+        static::assertSame('POLYGON((-71.1776848522251 42.3902896512902,-71.1776843766326 42.3903829478009, -71.1775844305465 42.3903826677917,-71.1775825927231 42.3902893647987,-71.1776848522251 42.3902896512902))', $result[0][1]);
+        // phpcs: enable
     }
 }
