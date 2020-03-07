56e80724fc46d70a5b9ab8c8fde207a517c78fd9
Alexandre-T <alexandre.tranchant@gmail.com>
2020-03-07 10:12:28 +0100

    Adding new PostgreSQL test

---
+++ tests/CrEOF/Spatial/Tests/ORM/Query/AST/Functions/Standard/SpGeogFromTextTest.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * Copyright (C) 2020 Alexandre Tranchant
+ * Copyright (C) 2015 Derek J. Lambert
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace CrEOF\Spatial\Tests\ORM\Query\AST\Functions\PostgreSql;
+
+use CrEOF\Spatial\Exception\InvalidValueException;
+use CrEOF\Spatial\Exception\UnsupportedPlatformException;
+use CrEOF\Spatial\Tests\Helper\PointHelperTrait;
+use CrEOF\Spatial\Tests\OrmTestCase;
+use Doctrine\DBAL\DBALException;
+use Doctrine\ORM\ORMException;
+
+/**
+ * ST_GeogFromText DQL function tests.
+ *
+ * @author  Derek J. Lambert <dlambert@dereklambert.com>
+ * @author  Alexandre Tranchant <alexandre.tranchant@gmail.com>
+ * @license https://dlambert.mit-license.org MIT
+ *
+ * @group dql
+ *
+ * @internal
+ * @coversDefaultClass
+ */
+class StGeogFromTextTest extends OrmTestCase
+{
+    use PointHelperTrait;
+
+    /**
+     * Setup the function type test.
+     *
+     * @throws DBALException                when connection failed
+     * @throws ORMException                 when cache is not set
+     * @throws UnsupportedPlatformException when platform is unsupported
+     */
+    protected function setUp(): void
+    {
+        $this->usesEntity(self::GEOGRAPHY_ENTITY);
+        $this->supportsPlatform('postgresql');
+
+        parent::setUp();
+    }
+
+    /**
+     * Test a DQL containing function to test in the predicate.
+     *
+     * @throws DBALException                when connection failed
+     * @throws ORMException                 when cache is not set
+     * @throws UnsupportedPlatformException when platform is unsupported
+     * @throws InvalidValueException        when geometries are not valid
+     *
+     * @group geometry
+     */
+    public function testPredicate()
+    {
+        $newYork = $this->createNewYorkGeography();
+        $this->createLosAngelesGeography();
+        $this->createPointA();
+        $this->getEntityManager()->flush();
+        $this->getEntityManager()->clear();
+
+        $query = $this->getEntityManager()->createQuery(
+            'SELECT g FROM CrEOF\Spatial\Tests\Fixtures\GeographyEntity g WHERE g.geography = PgSQL_GeogFromText(:g)'
+        );
+        $query->setParameter('g', 'SRID=4326;POINT(-73.938611 40.664167)');
+
+        $result = $query->getResult();
+
+        static::assertCount(1, $result);
+        static::assertEquals($newYork, $result[0]);
+    }
+}
